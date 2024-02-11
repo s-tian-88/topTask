dbb48eb7f6dbe872ac5eff3edaf92e53058e0c97
Sergei Tian <s.tian.spb@gmail.com>
2024-02-11 16:58:37 +0300

    delete all)

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-import AddWidget from '../components/add-widget/add-widget';
-import FilterWidget from '../components/filter-widget/filter-widget';
-
-const container = document.createElement('div');
-container.classList.add('tasks-container');
-document.body.appendChild(container);
-
-const addWidget = new AddWidget(container);
-const filterWidget = new FilterWidget(container);
